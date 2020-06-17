c62f49abce887ab290d98938ab13edd0d3645836
Anu2006-dev <anu2006developer@gmail.com>
2020-06-17 15:48:21 +0530

    errors fixed

---
+++ src/components/project_item.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class ProjectItem extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+class Projects extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Projects;
