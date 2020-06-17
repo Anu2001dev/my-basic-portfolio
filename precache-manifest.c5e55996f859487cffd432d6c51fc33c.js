434364b57918b528b68d619acb0c632a8ae7b6cb
Anu2006-dev <anu2006developer@gmail.com>
2020-06-17 15:07:19 +0530

    Updates

---
+++ precache-manifest.c5e55996f859487cffd432d6c51fc33c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ba1790e2b152b344f3718749b4b72e",
+    "url": "/my-basic-portfolio/index.html"
+  },
+  {
+    "revision": "17cd597d9cb3974a516d",
+    "url": "/my-basic-portfolio/static/css/main.01659bd7.chunk.css"
+  },
+  {
+    "revision": "03d65cfe2ff6a7f131a0",
+    "url": "/my-basic-portfolio/static/js/2.4e63a2bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-basic-portfolio/static/js/2.4e63a2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17cd597d9cb3974a516d",
+    "url": "/my-basic-portfolio/static/js/main.05cf8f2d.chunk.js"
+  },
+  {
+    "revision": "137de2d0bece3059fbee",
+    "url": "/my-basic-portfolio/static/js/runtime-main.610f70a4.js"
+  },
+  {
+    "revision": "33172fdd10aa029bb09d1503568897c0",
+    "url": "/my-basic-portfolio/static/media/anime.33172fdd.svg"
+  },
+  {
+    "revision": "0956c73f3ad1f14c20624063900a73c3",
+    "url": "/my-basic-portfolio/static/media/background.0956c73f.svg"
+  },
+  {
+    "revision": "2c01c6df565ec05b89e2faa93aac8da2",
+    "url": "/my-basic-portfolio/static/media/profile pic.2c01c6df.svg"
+  }
+]);
